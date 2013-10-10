13a642f79b83dd07d20cb598d118844c6e8adcfc
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-10 09:52:36 -0500

    Model work

--- db/migrate/20131010081218_create_users.rb
+++ db/migrate/20131010081218_create_users.rb
@@ -4,7 +4,6 @@ class CreateUsers < ActiveRecord::Migration
         t.string :username, :null => false
         t.string :email, :null => false
         t.string :password_hash, :null => false
-        t.integer :post_id
         t.timestamps
       end
   end
