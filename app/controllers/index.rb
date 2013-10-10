c61a07137cc56287d4bb39f3b31fdbabe1474fbf
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-10 15:28:19 -0500

    routing issues with new posts

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,40 +5,45 @@ get '/' do
   erb :index
 end
 
+#######OK
 get '/login' do
   erb :index
 end
 
+#######OK
 get '/all' do
   @allposts = Post.all
   erb :all
 end
 
+#######OK
 get '/profile/:user_id' do
   @user = User.find(params[:user_id])
   erb :profile
 end
 
-
+######OK
 get '/all/:user_id' do
   @user = User.find(params[:user_id])
   @user_posts = @user.posts
   erb :alluser
 end
 
-get '/post/:post_id' do
+
+get '/:post_id' do############################################
   @post = Post.find(params[:post_id])
   erb :singlepost
 end
 
-get '/newpost' do
-  if current_user
+get '/newpost' do####not taking sessions
+  if current_user###################################################
     @user_id = current_user.id
    erb :new_post
   else
     redirect '/login'
   end
 end
+
 
 get '/logout' do
   session.clear
@@ -47,6 +52,7 @@ end
 
 # POST ==========================================
 
+#######OK - able to login
 post '/login' do
     @user = User.find_by_email(params[:email])
     if @user.password == params[:password]
@@ -65,11 +71,12 @@ post '/post/:post_id' do
 end
 
 post '/newpost/:user_id' do
-    @user_id = params[:user_id] #kinda overkill
+    @user_id = params[:user_id]
     @post = Post.create(title: params[:title], url: params[:url], user_id: @user_id)
-  redirect '/'
+  redirect "all/#{@user_id}"
 end
 
+########OK
 post '/newuser' do
   @user = User.create(params[:newuser])
   redirect "/profile/#{@user.id}"
